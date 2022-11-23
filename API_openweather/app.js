1696c89b392bc74bc04130b682a4a47f0835a6b2
jorgeEF <jorgefemenia@gmail.com>
2022-11-23 14:42:31 -0300

    quitada api key del repositorio publico

--- API_openweather/app.js
+++ API_openweather/app.js
@@ -4,7 +4,7 @@ const request = require('request');
 const app = express();
 
 app.get("/", (req,res) => {        
-    request('https://api.openweathermap.org/data/2.5/weather?id=3861445&appid=d151fb90208c92b784e87974758bd465&lang=sp&units=metric', function (error, response, body) {
+    request('https://api.openweathermap.org/data/2.5/weather?id={city}&appid={API KEY}&lang=sp&units=metric', function (error, response, body) {
     let data = JSON.parse(body)    
     if(response.statusCode === 200){
         res.send(`En ${data.name} la temperatura es de ${data.main.temp}ºC`)
--- API_openweather/app2.js
+++ API_openweather/app2.js
@@ -3,7 +3,7 @@ const https = require('https')
 
 const app = express();
 
-const api = "https://api.openweathermap.org/data/2.5/weather?id=3861445&appid=d151fb90208c92b784e87974758bd465&lang=sp&units=metric";
+const api = "https://api.openweathermap.org/data/2.5/weather?id={city}&appid={API KEY}&lang=sp&units=metric";
 
 app.get("/", (req,res) => {          
     https.get(api, function(response) {        
